f3b5b1571d4c4725104a17a98a7a8921548a6c02
marctex <marcelo.t.martins@hotmail.com>
2021-06-21 16:43:23 -0300

    upd first modification

---
+++ gitProjectUnityBall/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+        rb.AddForce(move * velocity);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("item"))
+        {
+            Destroy(other.gameObject);
+        }
+       
+    }
+}
---
+++ gitProjectUnityBall/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public GameObject objectBall;
+    private Vector3 positionInitial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionInitial = transform.position - objectBall.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = objectBall.transform.position + positionInitial;
+    }
+}
